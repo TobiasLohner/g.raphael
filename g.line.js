233670faa15eef2200f35182973dcef6bc9c4e6d
Tobias Lohner <tobias@lohner-net.de>
2012-06-11 12:18:55 +0200

    g.line.js: keep original values[x|y] and work on shrinked copies of them only

--- g.line.js
+++ g.line.js
@@ -80,23 +80,26 @@
 
         var shades = createShades();
 
+        var valuesx_shrinked = [];
+        var valuesy_shrinked = [];
+
         for (i = 0, ii = valuesy.length; i < ii; i++) {
             if (valuesy[i].length > width - 2 * gutter) {
-                valuesy[i] = shrink(valuesy[i], width - 2 * gutter);
+                valuesy_shrinked[i] = shrink(valuesy[i], width - 2 * gutter);
                 len = width - 2 * gutter;
             }
 
             if (valuesx[i] && valuesx[i].length > width - 2 * gutter) {
-                valuesx[i] = shrink(valuesx[i], width - 2 * gutter);
+                valuesx_shrinked[i] = shrink(valuesx[i], width - 2 * gutter);
             }
         }
 
-        var allx = Array.prototype.concat.apply([], valuesx),
-            ally = Array.prototype.concat.apply([], valuesy),
-            xdim = chartinst.snapEnds(Math.min.apply(Math, allx), Math.max.apply(Math, allx), valuesx[0].length - 1),
+        var allx = Array.prototype.concat.apply([], valuesx_shrinked),
+            ally = Array.prototype.concat.apply([], valuesy_shrinked),
+            xdim = chartinst.snapEnds(Math.min.apply(Math, allx), Math.max.apply(Math, allx), valuesx_shrinked[0].length - 1),
             minx = xdim.from,
             maxx = xdim.to,
-            ydim = chartinst.snapEnds(Math.min.apply(Math, ally), Math.max.apply(Math, ally), valuesy[0].length - 1),
+            ydim = chartinst.snapEnds(Math.min.apply(Math, ally), Math.max.apply(Math, ally), valuesy_shrinked[0].length - 1),
             miny = ydim.from,
             maxy = ydim.to,
             kx = (width - gutter * 2) / ((maxx - minx) || 1),
@@ -111,7 +114,7 @@
         function createShades() {
             var shades = paper.set();
 
-            for (i = 0, ii = valuesy.length; i < ii; i++) {
+            for (i = 0, ii = valuesy_shrinked.length; i < ii; i++) {
                 if (opts.shade) {
                     shades.push(paper.path().attr({ stroke: "none", fill: colors[i], opacity: opts.nostroke ? 1 : .3 }));
                 }
@@ -137,7 +140,7 @@
                 symbols = paper.set(),
                 line;
 
-            for (i = 0, ii = valuesy.length; i < ii; i++) {
+            for (i = 0, ii = valuesy_shrinked.length; i < ii; i++) {
                 if (!opts.nostroke) {
                     lines.push(line = paper.path().attr({
                         stroke: colors[i],
@@ -153,17 +156,17 @@
 
                 path = [];
 
-                for (var j = 0, jj = valuesy[i].length; j < jj; j++) {
-                    var X = x + gutter + ((valuesx[i] || valuesx[0])[j] - minx) * kx,
-                        Y = y + height - gutter - (valuesy[i][j] - miny) * ky;
+                for (var j = 0, jj = valuesy_shrinked[i].length; j < jj; j++) {
+                    var X = x + gutter + ((valuesx_shrinked[i] || valuesx_shrinked[0])[j] - minx) * kx,
+                        Y = y + height - gutter - (valuesy_shrinked[i][j] - miny) * ky;
 
                     (Raphael.is(sym, "array") ? sym[j] : sym) && symset.push(paper[Raphael.is(sym, "array") ? sym[j] : sym](X, Y, (opts.width || 2) * 3).attr({ fill: colors[i], stroke: "none" }));
                     if (opts.smooth) {
                         if (j && j != jj - 1) {
-                            var X0 = x + gutter + ((valuesx[i] || valuesx[0])[j - 1] - minx) * kx,
-                                Y0 = y + height - gutter - (valuesy[i][j - 1] - miny) * ky,
-                                X2 = x + gutter + ((valuesx[i] || valuesx[0])[j + 1] - minx) * kx,
-                                Y2 = y + height - gutter - (valuesy[i][j + 1] - miny) * ky,
+                            var X0 = x + gutter + ((valuesx_shrinked[i] || valuesx_shrinked[0])[j - 1] - minx) * kx,
+                                Y0 = y + height - gutter - (valuesy_shrinked[i][j - 1] - miny) * ky,
+                                X2 = x + gutter + ((valuesx_shrinked[i] || valuesx_shrinked[0])[j + 1] - minx) * kx,
+                                Y2 = y + height - gutter - (valuesy_shrinked[i][j + 1] - miny) * ky,
                                 a = getAnchors(X0, Y0, X, Y, X2, Y2);
 
                             path = path.concat([a.x1, a.y1, X, Y, a.x2, a.y2]);
@@ -184,7 +187,7 @@
                 symbols.push(symset);
 
                 if (opts.shade) {
-                    shades[i].attr({ path: path.concat(["L", X, y + height - gutter, "L",  x + gutter + ((valuesx[i] || valuesx[0])[0] - minx) * kx, y + height - gutter, "z"]).join(",") });
+                    shades[i].attr({ path: path.concat(["L", X, y + height - gutter, "L",  x + gutter + ((valuesx_shrinked[i] || valuesx_shrinked[0])[0] - minx) * kx, y + height - gutter, "z"]).join(",") });
                 }
 
                 !opts.nostroke && line.attr({ path: path.join(",") });
@@ -197,8 +200,8 @@
             // unite Xs together
             var Xs = [];
 
-            for (var i = 0, ii = valuesx.length; i < ii; i++) {
-                Xs = Xs.concat(valuesx[i]);
+            for (var i = 0, ii = valuesx_shrinked.length; i < ii; i++) {
+                Xs = Xs.concat(valuesx_shrinked[i]);
             }
 
             Xs.sort();
@@ -228,13 +231,13 @@
                 C.x = xs[i];
                 C.axis = Xs[i];
 
-                for (var j = 0, jj = valuesy.length; j < jj; j++) {
-                    Xs2 = valuesx[j] || valuesx[0];
+                for (var j = 0, jj = valuesy_shrinked.length; j < jj; j++) {
+                    Xs2 = valuesx_shrinked[j] || valuesx_shrinked[0];
 
                     for (var k = 0, kk = Xs2.length; k < kk; k++) {
                         if (Xs2[k] == Xs[i]) {
-                            C.values.push(valuesy[j][k]);
-                            C.y.push(y + height - gutter - (valuesy[j][k] - miny) * ky);
+                            C.values.push(valuesy_shrinked[j][k]);
+                            C.y.push(y + height - gutter - (valuesy_shrinked[j][k] - miny) * ky);
                             C.symbols.push(chart.symbols[j][k]);
                         }
                     }
@@ -250,21 +253,21 @@
             var cvrs = f || paper.set(),
                 C;
 
-            for (var i = 0, ii = valuesy.length; i < ii; i++) {
-                for (var j = 0, jj = valuesy[i].length; j < jj; j++) {
-                    var X = x + gutter + ((valuesx[i] || valuesx[0])[j] - minx) * kx,
-                        nearX = x + gutter + ((valuesx[i] || valuesx[0])[j ? j - 1 : 1] - minx) * kx,
-                        Y = y + height - gutter - (valuesy[i][j] - miny) * ky;
+            for (var i = 0, ii = valuesy_shrinked.length; i < ii; i++) {
+                for (var j = 0, jj = valuesy_shrinked[i].length; j < jj; j++) {
+                    var X = x + gutter + ((valuesx_shrinked[i] || valuesx_shrinked[0])[j] - minx) * kx,
+                        nearX = x + gutter + ((valuesx_shrinked[i] || valuesx_shrinked[0])[j ? j - 1 : 1] - minx) * kx,
+                        Y = y + height - gutter - (valuesy_shrinked[i][j] - miny) * ky;
 
                     f ? (C = {}) : cvrs.push(C = paper.circle(X, Y, Math.abs(nearX - X) / 2).attr({ stroke: "none", fill: "#000", opacity: 0 }));
                     C.x = X;
                     C.y = Y;
-                    C.value = valuesy[i][j];
+                    C.value = valuesy_shrinked[i][j];
                     C.line = chart.lines[i];
                     C.shade = chart.shades[i];
                     C.symbol = chart.symbols[i][j];
                     C.symbols = chart.symbols[i];
-                    C.axis = (valuesx[i] || valuesx[0])[j];
+                    C.axis = (valuesx_shrinked[i] || valuesx_shrinked[0])[j];
                     f && f.call(C);
                 }
             }
